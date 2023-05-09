5c7cff70265ccefdd96bccac9c64c6127f9b3096
Ilham Fauzi <muh.ilhamfz@gmail.com>
2023-05-09 23:23:20 +0700

    chore: update dashboard & detail

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -27,7 +27,61 @@ export default function Dashboard(props) {
             <Head title="Dashboard" />
             <div className="py-12">
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                    <Chart options={options} type="bar" series={series} width="80%" />
+                    <div className='flex gap-4 mb-4'>
+                        <div className='w-4/12'>
+                            <div className='px-9 py-5 bg-white overflow-hidden shadow-sm sm:rounded-lg'>
+                                <div className='flex flex-row items-center'>
+                                    <div className="h-14 w-14 mr-4 bg-red-50 dark:bg-red-800/20 flex items-center justify-center rounded-full">
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" className="w-6 h-6 stroke-red-500"><path strokeLinecap="round" strokeLinejoin="round" d="M12 6.042A8.967 8.967 0 006 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 016 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 016-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0018 18a8.967 8.967 0 00-6 2.292m0-14.25v14.25"></path></svg>
+                                    </div>
+                                    <div>
+                                        <p className='text-gray-500'>News Published</p>
+                                        <p className='font-semibold text-3xl text-gray-800'>1233000</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className='w-4/12'>
+                            <div className='px-9 py-5 bg-white overflow-hidden shadow-sm sm:rounded-lg'>
+                                <div className='flex flex-row items-center'>
+                                    <div className="h-14 w-14 mr-4 bg-red-50 dark:bg-red-800/20 flex items-center justify-center rounded-full">
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" className="w-6 h-6 stroke-red-500"><path strokeLinecap="round" strokeLinejoin="round" d="M12 6.042A8.967 8.967 0 006 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 016 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 016-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0018 18a8.967 8.967 0 00-6 2.292m0-14.25v14.25"></path></svg>
+                                    </div>
+                                    <div>
+                                        <p className='text-gray-500'>News Published</p>
+                                        <p className='font-semibold text-3xl text-gray-800'>1233000</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className='w-4/12'>
+                            <div className='px-9 py-5 bg-white overflow-hidden shadow-sm sm:rounded-lg'>
+                                <div className='flex flex-row items-center'>
+                                    <div className="h-14 w-14 mr-4 bg-red-50 dark:bg-red-800/20 flex items-center justify-center rounded-full">
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" className="w-6 h-6 stroke-red-500"><path strokeLinecap="round" strokeLinejoin="round" d="M12 6.042A8.967 8.967 0 006 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 016 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 016-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0018 18a8.967 8.967 0 00-6 2.292m0-14.25v14.25"></path></svg>
+                                    </div>
+                                    <div>
+                                        <p className='text-gray-500'>News Published</p>
+                                        <p className='font-semibold text-3xl text-gray-800'>1233000</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className='w-4/12'>
+                            <div className='px-9 py-5 bg-white overflow-hidden shadow-sm sm:rounded-lg'>
+                                <div className='flex flex-row items-center'>
+                                    <div className="h-14 w-14 mr-4 bg-red-50 dark:bg-red-800/20 flex items-center justify-center rounded-full">
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" className="w-6 h-6 stroke-red-500"><path strokeLinecap="round" strokeLinejoin="round" d="M12 6.042A8.967 8.967 0 006 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 016 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 016-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0018 18a8.967 8.967 0 00-6 2.292m0-14.25v14.25"></path></svg>
+                                    </div>
+                                    <div>
+                                        <p className='text-gray-500'>News Published</p>
+                                        <p className='font-semibold text-3xl text-gray-800'>1233000</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    {/* <Chart options={options} type="bar" series={series} width="100%" className="h-20" /> */}
                     <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                         <div className="p-6 text-gray-900">You're logged in!</div>
                     </div>
--- resources/js/Pages/Detail.jsx
+++ resources/js/Pages/Detail.jsx
@@ -4,7 +4,15 @@ import { Pagination } from '@/Components/Pagination';
 import Unauthenticated from '@/Layouts/UnauthenticatedLayout';
 import { Head } from '@inertiajs/react';
 export default function Detail(props) {
-    console.log(props.news)
+    const isValidUrl = (url) => {
+        try {
+            new URL(url);
+            return url;
+        } catch (e) {
+            return window.location.origin + '/uploads/' + url;
+        }
+    }
+
     return (
         <Unauthenticated
             auth={props.auth}
@@ -19,7 +27,7 @@ export default function Detail(props) {
                     </div>
                     <div className='flex'>
                         <div className='w-3/12 flex'>
-                            <img src='https://picsum.photos/1000/500?random=1' alt={props.news.author} className='w-12 h-12 mr-3 rounded-full' />
+                            <img src='https://picsum.photos/1000/500?random=1' alt={props.news.author} className='w-12 h-12 mr-3 rounded-full object-cover' />
                             <div>
                                 <p className='font-bold'>Author</p>
                                 <p className='mb-6'>{props.news.author}</p>
@@ -38,12 +46,12 @@ export default function Detail(props) {
             </div>
             <div className='relative h-0 sm:px-6 lg:px-8'>
                 <div className='absolute bottom-0 w-full'>
-                    <img src={props.news.media.url} className='h-96 w-fit object-cover' />
+                    <img src={isValidUrl(props.news.media.url)} className='h-96 w-fit object-cover' />
                 </div>
             </div>
             <div className='py-12'>
                 <div className="mx-auto sm:px-6 lg:px-8">
-                    <p>{props.news.content}</p>
+                    <div dangerouslySetInnerHTML={{ __html: props.news.content }} />
                 </div>
             </div >
         </Unauthenticated >
